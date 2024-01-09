9be955877f1a0b3e0fc8e818f4b303be83469b33
Noah Smith <noah.smith@stu.ocsb.ca>
2024-01-09 12:16:24 -0500

    code.py 01

--- code.py
+++ code.py
@@ -1,1 +1,23 @@
-# Noah
+#!/usr/bin/env python3
+
+# Created by: Noah Smith
+# Created on: January 8th, 2024
+# This program is the "Space Aliens" program on the PyBadge
+
+import stage
+import ugame
+
+
+def game_scene():
+    # this function is the main game game_scene
+
+    print("\n\n\n") # 3 blank lines
+    print("Hello, Noah!")
+    print("It is cold outside!")
+
+    # repeat forever, game loop
+    while True:
+        pass # placeholder
+
+if__name__ == "__main__":
+    game_scene()
