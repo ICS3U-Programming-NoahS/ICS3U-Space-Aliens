017ff555d3ba927f2f082f8626d63240cfa3411f
Noah Smith <noah.smith@stu.ocsb.ca>
2024-01-11 12:11:44 -0500

    created CPT 03

--- code_1.py
+++ code_1.py
@@ -13,10 +13,14 @@ def game_scene():
 
     # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp]")
 
     # Set the background to image 0 in the image bank
     #   and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
+
+    # a sprite that will be updated every frame
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
     # Create a stage for the background to show up on
     #   and set the frame rate to 60fps
