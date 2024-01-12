741be133c519e5867fbd1bf7eb09235e0b42025d
Noah Smith <noah.smith@stu.ocsb.ca>
2024-01-12 12:27:26 -0500

    created CPT 05

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#
+# Created by: Noah Smith
+# Created on: January 10th, 2024
+# This constants file is for "Space Aliens" program on the PyBadge
+
+# PyBadge screen size is 150x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
